fbae5acafdddce213c299add53d9c3f7dc5feeb5
Ali Umar <ali.umar.work@gmail.com>
2021-08-20 18:22:25 -0400

    initial project structure

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Contact: NextPage = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
+
+
---
+++ pages/skills.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Skills: NextPage = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
+
+
