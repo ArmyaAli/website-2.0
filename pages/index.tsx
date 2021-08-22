1de0952ab46704f43b73742290bad1c284e0da53
Ali Umar <ali.umar.work@gmail.com>
2021-08-22 18:40:19 -0400

    adding intro, interests,skills,projects and unfinished footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,20 @@ import Image from 'next/image'
 import React from 'react'
 import Footer from '../components/footer'
 import Navbar from '../components/navbar'
-import styles from '../styles/Home.module.css'
+import Interests from '../landing-content/interests'
+import Intro from '../landing-content/intro'
+import Projects from '../landing-content/projects'
+import Skills from '../landing-content/skills'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Navbar></Navbar>
-      {/* <Footer></Footer> */}
+      <Intro></Intro>
+      <Interests></Interests>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Footer></Footer>
     </div>
   )
 }
