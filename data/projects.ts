4b419d7114bd60c286c328e7e4d8dcc5219b006c
Ali Umar <ali.umar.work@gmail.com>
2021-08-22 18:38:10 -0400

    projects data template

---
+++ data/projects.ts
@@ -0,0 +1,20 @@
+const PROJECTS = [
+    {
+        title: "Project 1",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+    },
+    {
+        title: "Project 1",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+    },
+    {
+        title: "Project 1",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+    },
+    {
+        title: "Project 1",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+    }
+];
+
+export default PROJECTS
