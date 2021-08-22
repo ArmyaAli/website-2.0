c7e2bd4f0bf8e6691e919812d6aeebaeb16e963c
Ali Umar <ali.umar.work@gmail.com>
2021-08-22 18:35:53 -0400

    created headshot component + styling

---
+++ components/headshot.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Headshot.module.css'
+
+const Headshot = () => {
+    return (
+        <div className={styles.profileContainer}>
+           <img className={styles.profileImage} src="profile2.jpg" alt="Picture of my face"/> 
+        </div>
+    )
+}
+
+export default Headshot
+
+
+
+
