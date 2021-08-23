04ccbfbb0d0aee628ead7314ff4606535f38408a
Ali Umar <ali.umar.work@gmail.com>
2021-08-23 18:04:32 -0400

    added navbar and footer to notes page

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
+import Footer from '../components/footer'
+import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
 
 const Notes: NextPage = () => {
   return (
-    <div>Notes</div>
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
   )
 }
 
