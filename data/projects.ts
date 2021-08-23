c8c8f77fbeb055a10ad0aebe8f0e13b60ed47653
Ali Umar <ali.umar.work@gmail.com>
2021-08-23 10:11:53 -0400

    updated placeholder information with useful text blurbs

--- data/projects.ts
+++ data/projects.ts
@@ -1,19 +1,19 @@
 const PROJECTS = [
     {
-        title: "Project 1",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+        title: "Mushroom Bot",
+        desc: "A Discord Bot hosted on my Raspberry PI! It features a music player, coin flip, Urban Dictionary lookup and various other cool stuff. I built a CI pipeline using Github Actions to auto deploy to my Raspberry PI for my personal dev environment."
     },
     {
-        title: "Project 1",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+        title: "Bestbuy Scalper",
+        desc: "This got me banned off the Canadian BestBuy website temporarily. It uses a tool called puppeteer to automate a chrome browser instance. Utilizing automation you are able to check if a product is in stock. You can feed it a text file containing product links and it'll get to work. I was attempting to use it to scalp RTX 3080 cards."
     },
     {
-        title: "Project 1",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+        title: "Shopping Solution - Windows Forms",
+        desc: "This was the final project of a course I took at Seneca College in my last semester. It is a boring old Client/Server application that utilizes a variety of features C# and .NET have to offer. Asynchronous programming, multi-threading, .NET sockets APIs. It is named Ali's Tea Shop and it was quite fun to build out. I also got a good mark!"
     },
     {
-        title: "Project 1",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est perspiciatis odio deleniti exercitationem, molestiae natus nulla a alias eveniet sed in aut laudantium iste quae veniam beatae corporis non repellat repellendus quaerat maiores perferendis magnam? Optio impedit cupiditate numquam ea eligendi molestiae similique accusamus mollitia alias laudantium. Voluptate, ut!"
+        title: "Mushroom Fighter",
+        desc: "This was intended to be some kind of game in the end, but it never really went anywhere. I used GCC for windows to compile the program, written entirely in C and using SFML library for graphics and media. I managed to get a barebones menu and a sprite moving around on the screen. Observe the GIF above for a better idea. This was also fun to make!"
     }
 ];
 
--- landing-content/intro.tsx
+++ landing-content/intro.tsx
@@ -12,7 +12,7 @@ const Intro = () => {
           <div className={styles.profileText}>
               <div className={styles.profileName}>Ali Umar</div>
               <div className={styles.profileTitle}>Software Developer</div>
-              <p className={styles.profileDesc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga enim perspiciatis magnam saepe facilis cupiditate omnis officiis consectetur facere earum! Magni dolorem officiis dolore! At, enim beatae! Ipsam itaque, culpa quibusdam quia officiis accusantium obcaecati rem assumenda pariatur autem voluptate quisquam eius impedit, cum atque amet ex illum voluptas iure tempora repellendus porro qui vel. Voluptatum facilis quia error recusandae laudantium reprehenderit quas eaque tempore ratione blanditiis ex, explicabo mollitia ducimus. Eius recusandae rem quis, pariatur esse voluptas similique minima? Qui fugiat sit dignissimos ut error quisquam dolorem dolor rerum, hic accusamus tenetur vel voluptate eos consequatur doloribus enim quo?</p>
+              <p className={styles.profileDesc}>Hi, my name is Ali Umar and I currently reside in Toronto, ON, Canada. I have a diploma in Computer Engineering Technology from Seneca College and I'm currently pursuing a bachelors in Computer Science at York University. I like programming, going to the gym and eating different kinds of food. I thought I might try making a website with one of the many different web frameworks out there and here it is. I usually spend my days playing games, running errands and doing schoolwork. If you'd like to contact me, the best way would be to hit up my discord @LordMushroom#6459.</p>
           </div>
         </div>
     )
