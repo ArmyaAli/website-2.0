9d404f34c0e4107007af9865e173ea60d5f0a7f2
Ali Umar <ali.umar.work@gmail.com>
2021-08-23 08:26:40 -0400

    created basic footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Footer.module.css'
+import { AiOutlineTwitter, AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footerContainer}>
+      <div className={styles.top}>
+        <div className={styles.icon}><a href="https://ca.linkedin.com/in/aliumartech"><AiOutlineLinkedin className={styles.rIcon} size="64" /></a></div>
+        <div className={styles.icon}><a href="https://twitter.com/I_MainDarius"><AiOutlineTwitter className={styles.rIcon} size="64" /></a></div>
+        <div className={styles.icon}><a href="https://github.com/ArmyaAli"><AiOutlineGithub className={styles.rIcon} size="64" /></a></div>
+      </div>
+      <hr />
+      <div className={styles.bot}>
+        <div>&copy; Copyright Ali Umar</div>
+        <div>Built with NextJS</div>
+        <div>Hosted on Github Pages</div>
+      </div>
+    </div>
   )
 }
 
