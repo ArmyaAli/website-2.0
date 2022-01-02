5b22e9749480e074e96d76ca4a6df7a36f62e8d0
Ali Umar <ali.umar.work@gmail.com>
2022-01-02 10:54:24 -0500

    updated discord bot link

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -2,7 +2,7 @@ const NOTES = [
     {
         title: "Mushroom Bot",
         body: "Click the link below to add Mushroom Bot to your discord server.",
-        link: "https://discord.com/api/oauth2/authorize?client_id=783387766674161734&permissions=8&scope=bot"
+        link: "https://discord.com/api/oauth2/authorize?client_id=926247834423029811&permissions=8&scope=bot"
     },
 ];
 
